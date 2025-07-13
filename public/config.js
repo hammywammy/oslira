12288dd73b542c516cc0b4574c7007a4cad88792
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-07-12 23:29:25 -0700

    Delete public/config.js

--- public/config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-    supabaseUrl: 'https://jswzzihuqtjqvobfosks.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impzd3p6aWh1cXRqcXZvYmZvc2tzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyNTc2NDQsImV4cCI6MjA2NzgzMzY0NH0.Cz909ngJHcBkxp1aeFUn-ZvCrKRFf3phtGlrBvTCvWg'
-    workerUrl: 'https://ai-outreach-api.hamzawilliamsbusiness.workers.dev'
-};
