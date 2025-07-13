b8c4fa92cab5b01225da21b5d3e27d860f0b6fa2
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-07-12 19:52:31 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    supabaseUrl: 'https://jswzzihuqtjqvobfosks.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impzd3p6aWh1cXRqcXZvYmZvc2tzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIwMjQxMzUsImV4cCI6MjA2NzYwMDEzNX0.XMoC3iLcDbKNxPfhTHj8CQsEjelTIrivIddfPTO9P64'
+};
