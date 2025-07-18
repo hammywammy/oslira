beecac2c98fede4d5f3e730f9bc8d72958ba5857
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-07-18 13:23:37 -0700

    Create cacheKeys.js

---
+++ public/analytics/utils/cacheKeys.js
@@ -0,0 +1,17 @@
+const CACHE_KEYS = {
+    MESSAGE_MATRIX: 'analytics_message_matrix',
+    CONVERSION_HEATMAP: 'analytics_conversion_heatmap',
+    CTA_TRACKER: 'analytics_cta_tracker',
+    FEEDBACK_EXPLORER: 'analytics_feedback_explorer',
+    CRM_COMPARATOR: 'analytics_crm_comparator',
+    TIMELINE_OVERLAY: 'analytics_timeline_overlay',
+    ITERATION_TRACKER: 'analytics_iteration_tracker',
+    TEAM_DASHBOARD: 'analytics_team_dashboard',
+    CLAUDE_GUIDANCE: 'analytics_claude_guidance',
+    RISK_CLASSIFIER: 'analytics_risk_classifier',
+    USER_PROFILE: 'analytics_user_profile',
+    BUSINESS_DATA: 'analytics_business_data',
+    FILTER_STATE: 'analytics_filter_state',
+    EXPORT_CONFIG: 'analytics_export_config',
+    FEATURE_FLAGS: 'analytics_feature_flags'
+};
