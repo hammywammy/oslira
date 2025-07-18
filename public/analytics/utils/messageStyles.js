ac0bf4a47962cef18b7eee229fdf166333ea1b70
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-07-18 13:23:12 -0700

    Update messageStyles.js

--- public/analytics/utils/messageStyles.js
+++ public/analytics/utils/messageStyles.js
@@ -79,28 +79,6 @@ const MESSAGE_STYLES = {
     }
 };
 
-
-
-// Data refresh and cache management constants
-const CACHE_KEYS = {
-    MESSAGE_MATRIX: 'analytics_message_matrix',
-    CONVERSION_HEATMAP: 'analytics_conversion_heatmap',
-    CTA_TRACKER: 'analytics_cta_tracker',
-    FEEDBACK_EXPLORER: 'analytics_feedback_explorer',
-    CRM_COMPARATOR: 'analytics_crm_comparator',
-    TIMELINE_OVERLAY: 'analytics_timeline_overlay',
-    ITERATION_TRACKER: 'analytics_iteration_tracker',
-    TEAM_DASHBOARD: 'analytics_team_dashboard',
-    CLAUDE_GUIDANCE: 'analytics_claude_guidance',
-    RISK_CLASSIFIER: 'analytics_risk_classifier',
-    USER_PROFILE: 'analytics_user_profile',
-    BUSINESS_DATA: 'analytics_business_data',
-    FILTER_STATE: 'analytics_filter_state',
-    EXPORT_CONFIG: 'analytics_export_config',
-    FEATURE_FLAGS: 'analytics_feature_flags'
-};
-
-// Export and reporting configuration
 const EXPORT_CONFIG = {
     formats: {
         pdf: {
