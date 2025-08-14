9d15fa818d2cf90cbf030d9ac42fc8d6fe4cde4b
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-08-14 13:16:00 -0700

    Update analytics.ts

--- cloudflare-workers/src/handlers/analytics.ts
+++ cloudflare-workers/src/handlers/analytics.ts
@@ -2,6 +2,7 @@ import type { Context } from 'hono';
 import { logger } from '../utils/logger.js';
 import { getAnalyticsSummary, generateAIInsights } from '../services/analytics.js';
 import { fetchJson, callWithRetry } from '../utils/helpers.js';
+import type { Env } from '../types/interfaces.js';
 
 export async function handleAnalyticsSummary(c: Context): Promise<Response> {
   try {
