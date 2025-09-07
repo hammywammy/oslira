7b5f4b65a568c2017537ba617c5f3899e1409191
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-09-06 18:21:33 -0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}",
+    "./public/pages/**/*.{html,js,css}",
+    "./public/core/**/*.{html,js,css}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'oslira-blue': '#2D6CDF',
+        'oslira-purple': '#8A6DF1'
+      }
+    }
+  },
+  plugins: []
+}
